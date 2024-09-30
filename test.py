41375c17130f984e1fd11900aee2a1c1e9adca2a
HaydenMarchinek <hayden.marchinek@icloud.com>
2024-09-30 12:40:45 -0700

    Update test.py
    Added serial number

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ Created on Tue Sep 24 14:12:04 2024
 import pylablib as pll
 pll.par["devices/dlls/picam"] = "path/to/dlls"
 from pylablib.devices import PrincetonInstruments
-cam1 = PrincetonInstruments.PicamCamera('number')
+cam1 = PrincetonInstruments.PicamCamera('0809080002')
 cam1.get_attribute_value("Pixel Format")  # get the current pixel format
 cam1.set_attribute_value("Exposure Time", 10)  # set the exposure time to 10 ms
 cam1.get_attribute_value("Exposure Time")
