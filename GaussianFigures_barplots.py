64de413803950564c7332b84a9f12aa75b42dd93
HaydenMarchinek <hayden.marchinek@icloud.com>
2024-10-15 19:03:09 -0700

    Update GaussianFigures_barplots.py

--- GaussianFigures_barplots.py
+++ GaussianFigures_barplots.py
@@ -1,31 +1,25 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Oct 11 12:07:46 2024
-
-@author: hayde
-"""
 
 import numpy as np
 import matplotlib.pyplot as plt
 
 # Define Gaussian function
 sigma = 5
-x, y = np.meshgrid(np.linspace(-1, 1, 100), np.linspace(-1, 1, 100))
+x, y = np.meshgrid(np.linspace(-1, 1, 1024), np.linspace(-1, 1, 1024))
 gaussian = np.exp(-(x**2 + y**2) / (2 * sigma**2))
 
 # Create the histogram values for y
 y_hist_values = []
-for j in range(100):
+for j in range(1024):
     y_sum = 0
-    for i in range(100):
+    for i in range(1024):
         y_sum += gaussian[i, j]
     y_hist_values.append(y_sum)
 
 # Create the histogram values for x
 x_hist_values = []
-for j in range(100):
+for j in range(1024):
     x_sum = 0
-    for i in range(100):
+    for i in range(1024):
         x_sum += gaussian[j, i]
     x_hist_values.append(x_sum)
 
@@ -40,17 +34,19 @@ ax_center.set_title('Gaussian Heatmap')
 
 # Add the x bar plot on top of the heatmap
 ax_top = fig.add_subplot(grid[0, :2])
-ax_top.bar(np.linspace(1, 100, 100), x_hist_values, width=1.0, color = 'red')  # Use bar plot instead of line plot
+ax_top.bar(np.linspace(-1, 1, 1024), x_hist_values, width=1.0, color = 'green')  # Use bar plot instead of line plot
 ax_top.set_title('X Intensity')
-ax_top.set_xlabel('X')
 ax_top.set_ylabel('Intensity')
+ax_top.set_ylim(1010,1020)
+ax_top.set_xlim(-1,1)
 
 # Add the y bar plot to the right of the heatmap
 ax_right = fig.add_subplot(grid[1:, 2])
-ax_right.barh(np.linspace(1, 100, 100), y_hist_values, height=1.0, color = 'red')  # Use horizontal bar plot
+ax_right.barh(np.linspace(-1, 1, 1024), y_hist_values, height=1.0, color = 'green')  # Use horizontal bar plot
 ax_right.set_title('Y Intensity')
 ax_right.set_xlabel('Intensity')
-ax_right.set_ylabel('Y')
+ax_right.set_xlim(1010,1020)
+ax_right.set_ylim(-1,1)
 
 # Show the combined figure
 plt.show()
